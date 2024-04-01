41f52b9cb5b805e8be1a6b6c4d74af153452ee72
tutorialshore <benfitsvideos@gmail.com>
2024-04-01 15:53:35 +0530

    new

--- backend/settings.py
+++ backend/settings.py
@@ -88,25 +88,25 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'landshield',
-        'USER': 'root',
-        'PASSWORD': 'lQkz?O=kn~z&',
-        'HOST': 'localhost',  # Or the IP address of your MySQL server
-        'PORT': '3306',       # Default MySQL port
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'landshield',
+#         'USER': 'root',
+#         'PASSWORD': 'lQkz?O=kn~z&',
+#         'HOST': 'localhost',  # Or the IP address of your MySQL server
+#         'PORT': '3306',       # Default MySQL port
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     }
+# }
 
 
 # Password validation
