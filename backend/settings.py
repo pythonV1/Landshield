2bc016a122ab7dbbe2691c9f4295f30ae8b5910a
tutorialshore <benfitsvideos@gmail.com>
2024-04-22 13:00:12 +0530

    changes

--- backend/settings.py
+++ backend/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'fmb_pdfs')
 MEDIA_URL = '/'
 
